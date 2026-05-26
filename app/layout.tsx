f83c4a86c72d6183fca4c0d745a9a21eb15ede56
Jayant Kumar <jkusseoni@gmail.com>
2026-05-26 17:08:50 +0530

    fix: update middleware + clerk provider config

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider afterSignOutUrl="/" signInUrl="/sign-in" signUpUrl="/sign-up">
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
